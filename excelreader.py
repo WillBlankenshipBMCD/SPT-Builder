eb853d14cbc04218aee608eafc9308958046240d
WillBlankenshipBMCD <53271712+WillBlankenshipBMCD@users.noreply.github.com>
2019-07-26 11:20:22 -0400

    Currently creates a full S9000 config

--- excelreader.py
+++ excelreader.py
@@ -1,21 +1,90 @@
 # all for importing edited excel doc
 
-#Extracting number of rows
-import xlrd
-file_loc = ("C:\Users\wblankenship\Documents\NIA Work Documents\FE\ASE SPT Protocol Translator Script\Book1.xlsx")
+# all for importing edited excel doc
 
-wb = xlrd.open_workbook(file_loc) 
-sheet = wb.sheet_by_index(0) 
+import xlrd
+
+file_loc = ("C:\Users\wblankenship\Documents\NIA Work Documents\FE\ASE SPT Protocol Translator Script\Book1.xlsx")
+wb = xlrd.open_workbook(file_loc)
+sheet = wb.sheet_by_name("Sheet1")
 rows = sheet.nrows
-b = [None] * 100
-	
-for i in range(sheet.nrows): 
-	a = str(sheet.cell_value(i, 1))
-	if a != "":
-		b[i] = a
-	print(b[i])
-	raw_input()
-		
+
+b = [None] * rows
+c = [None] * rows
+d = [None] * rows
+binarytags = [None] * rows
+controltags = [None] * rows
+analogtags = [None] * rows
+controlTagsOperations = [None] * rows
+
+rows2 = 0
+rows3 = 0
+
+# determing the number of filled rows to create row2, for control points. This is used for calculating how many inputs to add. This will have to be tested for different cases.
+for x in range(sheet.nrows):
+	cellValue = sheet.cell_value(x, 2)
+	if cellValue != "":
+		rows2 += 1
+
+# determing the number of filled rows to create row3. This is used for calculating how many inputs to add. This will have to be tested for different cases.
+for x in range(sheet.nrows):
+	cellValue = sheet.cell_value(x, 3)
+	if cellValue != "":
+		rows3 += 1
+
+# for determing the number of non empty cell values for the binary points column
+for x in range(sheet.nrows):
+	cellValue = sheet.cell_value(x, 1)
+	if cellValue != "":
+		b[x] = cellValue
+
+# assigning values to c[0...max] for control points
+# ********* there needs to be precautions put in if there is only one name instead of two
+for x in range(rows2 / 2):
+	cellValue = sheet.cell_value(x * 2, 2)
+	if cellValue != "":
+		c[x] = cellValue
+	else:
+		c[x] = None
+
+# assigning values to d[0...max] for analog points
+for x in range(rows3):
+	cellValue = sheet.cell_value(x, 3)
+	if cellValue != "":
+		d[x] = cellValue
+	else:
+		d[x] = None
+
+# collecting the control function from the book1 excel sheet
+for x in range(rows2):
+	cellValue = sheet.cell_value(x, 5)
+	if cellValue != "":
+		controlTagsOperations[x] = cellValue
+	else:
+		controlTagsOperations[x] = None
+
+# number of groups to create for the control points
+k = 0
+for x in range(rows2):
+	if (controlTagsOperations)
+
+# loops for creating the client side control points based on their name and functions
+for i in range(rows2):
+	if controlTagsOperations[i] = "DISABLE":
+		P5 = etree.Element("P", Id=str(i), Key=str(j), Name=str(binarytags[i]).replace(" ", "_").replace("\\", "_").replace("//", "_").replace("-", "_").upper())
+		P5.text = ""
+		P5.tail = "\n"
+		group1.insert(i, P5)
+
+tree = etree.ElementTree(root)
+tree.write("sample2.xml")
+# add for addtional info - - - > ,xml_declaration=True,   encoding="utf-8")
+
+
+
+
+
+
 		
 
 	
--- xmlwritertwo.py
+++ xmlwritertwo.py
@@ -3,7 +3,7 @@ from lxml import etree
 
 #Extracting number of rows
 import xlrd
-file_loc = ("C:\Users\wblankenship\Documents\NIA Work Documents\FE\ASE SPT Protocol Translator Script\Book1.xlsx")
+file_loc = ("C:\Users\wblankenship\Documents\GitHub\SPT-Builder\Book1.xlsx")
 wb = xlrd.open_workbook(file_loc)
 sheet = wb.sheet_by_name("Sheet1")
 rows = sheet.nrows
@@ -15,6 +15,10 @@ binarytags = [None] * rows
 controltags = [None] * rows
 analogtags = [None] * rows
 controlTagsOperations = [None] * rows
+# these lists are all used to keep track of the key values for all the points
+binaryNums = [None] * rows
+analogNums = [None] * rows
+controlNums = [None] * rows
 
 rows2 = 0
 rows3 = 0
@@ -25,7 +29,8 @@ for x in range(sheet.nrows):
 	if cellValue != "":
 		rows2 += 1
 
-# determing the number of filled rows to create row3. This is used for calculating how many inputs to add. This will have to be tested for different cases. 
+
+# determing the number of filled rows to create row3. This is used for calculating how many analog inputs to add. This will have to be tested for different cases.
 for x in range(sheet.nrows):
 	cellValue = sheet.cell_value(x,3)
 	if cellValue != "":
@@ -46,13 +51,6 @@ for x in range(rows2/2):
 	else:
 		c[x] = None
 
-# assigning values to d[0...max] for analog points
-for x in range(rows3):
-	cellValue = sheet.cell_value(x,3)
-	if cellValue != "":
-		d[x] = cellValue
-	else:
-		d[x] = None
 
 # collecting the control function from the book1 excel sheet
 for x in range(rows2):
@@ -137,6 +135,7 @@ device1.insert(0, object1)
 
 # loop for generating the binary tags on the server side
 i = 0
+# j is the variable that is used to increase the point ID for all the points on the Server and Client sides
 j = 118
 for i in range(rows):
 	if b[i] != None:
@@ -144,11 +143,13 @@ for i in range(rows):
 		P.text = ""
 		P.tail = "\n"
 		object1.insert(i, P)
+		binaryNums[i] = j
 	else: 
 		P = etree.Element("P", Id = str(i))
 		P.text = ""
 		P.tail = "\n"
 		object1.insert(i, P)
+		binaryNums[i] = j
 	j += 1
 
 
@@ -160,12 +161,12 @@ device1.insert(1, object2)
 
 
 i = 0
-j = 150
+h = 0
 k = 0
 # range(rows2/2)+1 because it doubles the name of each and there is ususally two rows for each point
 # this may have to be tweaked depending on whether the names are doubled or not
 for i in range((rows2/2)):
-	
+
 	if c[i] != None:
 		
 		P2 = etree.Element("P", Id = str(i))
@@ -177,16 +178,21 @@ for i in range((rows2/2)):
 		F1.text = ""
 		F1.tail = "\n"
 		P2.insert(k, F1)
-		j+=1
 		k+=1
-		
+		controlNums[h] = j
+		j += 1
+		h += 1
+
 		F2 = etree.Element("F", Id = "1", Ref = str(j))
 		F2.text = ""
 		F2.tail = "\n"
 		P2.insert(k, F2)
-		j+=1
-		k+=1	
-	else: 
+		k+=1
+		controlNums[h] = j
+		j += 1
+		h += 1
+
+	elif c[i] == "UNDEFINED":
 		
 		P2 = etree.Element("P", Id = str(i))
 		P2.text = "\n"
@@ -198,12 +204,18 @@ for i in range((rows2/2)):
 		F1.tail = "\n"
 		P2.insert(k, F1)
 		k+=1
+		j+=1
+		controlNums[h] = j
+		h += 1
 		
 		F2 = etree.Element("F", Id = "1")
 		F2.text = ""
 		F2.tail = "\n"
 		P2.insert(k, F2)
 		k+=1
+		j+=1
+		controlNums[h] = j
+		h += 1
 		
 	
 # start of Object 3 - for Analog Points ID = 30
@@ -213,26 +225,28 @@ object3.tail = "\n"
 device1.insert(2, object3)
 
 i = 0
-j = 134
-# points come in 4's. The other points get added in the client application side 
+
+# adding analog points on the server side
 for i in range(rows3):
 	if c[i] != None:
-		if i != 0 and (i%4) == 0:
-			j+=1
 		P3 = etree.Element("P", Id = str(i), Ref = str(j))
 		P3.text = ""
 		P3.tail = "\n"
 		object3.insert(i, P3)
-		
-	else: 
-		if i != 0 and (i%4) == 0:
-			j+=1
+		analogNums[i] = j
+	else:
 		P3 = etree.Element("P", Id = str(i))
 		P3.text = ""
 		P3.tail = "\n"
 		object3.insert(i, P3)
+		analogNums[i] = j
 		
 	j += 1
+
+#used for all the group and object keys. These keys do not reference back to the server side. They can be any arbitrary value.
+#I just started them off from where the point numbers that mattered ended.
+objGroupNums = j + 1
+j = 118
 
 #start of direction2 for client side portion
 direction2 = etree.Element("DIRECTION", Id = "1", Key = "111")
@@ -324,67 +338,175 @@ for x in range(sheet.nrows):
 
 # loop for generating the binary mcd/cd tags on the client side
 i = 0
-j = 118
+
 #used to start the next loop for the binary SS or NL points
-k = 0
+l = 0
 for i in range(rows):
 	#keeping track of where to start for the SS points, this loop comes last for all the points
-	k=i
-	#checks if we have reached the end of the CD type binaries
+	l=i
+	#checks if we have reached the end of the CD type binaries, this may need to be changed depending on order of points or point type
 	if b[i] == "NL":
 		break
 	#checks if the cell is either empty or a NL binary point
 	if b[i] != None and b[i] != "NL":
-		P4 = etree.Element("P", Id = str(i), Key = str(j), Name = str(binarytags[i]).replace(" ", "_").replace("\\","_").replace("//","_").replace("-","_").upper())
+		P4 = etree.Element("P", Id = str(i), Key = str(binaryNums[i]), Name = str(binarytags[i]).replace(" ", "_").replace("\\","_").replace("//","_").replace("-","_").upper())
 		P4.text = ""
 		P4.tail = "\n"
 		group1.insert(i, P4)
+
 	else: 
-		P4 = etree.Element("P", Id = str(i), Key = str(j))
+		P4 = etree.Element("P", Id = str(i), Key = str(binaryNums[i]))
 		P4.text = ""
 		P4.tail = "\n"
 		group1.insert(i, P4)
-	j += 1
+
+
 	
 #start of Object 5 - Analog Inputs ID = 2 for all binary inputs MCD and SS or NL
-object5 = etree.Element("OBJECT", Id = "2", Key = "132")
+object5 = etree.Element("OBJECT", Id = "2", Key = str(objGroupNums))
 object5.text = "\n"
 object5.tail = "\n"
 device2.insert(2, object5)
+objGroupNums+=1
+
+
+# for putting all the analog points in a list
+for x in range(rows3):
+	cellValue = sheet.cell_value(x,3)
+	if cellValue != "":
+		analogtags[x] = cellValue
+	else:
+		analogtags[x] = cellValue
 
 i = 0
 k = 0
 h = 0
-j = 133
+j = 0
 #loops for creating the client side analog points besed on their name and frame #
 #creating each frame or "GROUP"
 for i in range(rows3/4):
 
-	analogGroups = etree.Element("GROUP", Id = str(i), Key = str(j))
+	analogGroups = etree.Element("GROUP", Id = str(i), Key = str(objGroupNums))
 	analogGroups.text = "\n"
 	analogGroups.tail = "\n"
 	object5.insert(h,analogGroups)
+	objGroupNums += 1
 	
 	for h in range (4):
-		j+=1
-		P5 = etree.Element("P", Id = str(h), Key = str(j), Name = str(d[k]).replace(" ", "_").replace("\\","_").replace("//","_").replace("-","_").upper())
+
+		P5 = etree.Element("P", Id = str(h), Key = str(analogNums[j]), Name = str(analogtags[k]).replace(" ", "_").replace("\\","_").replace("//","_").replace("-","_").upper())
 		P5.text = ""
 		P5.tail = "\n"
 		analogGroups.insert(h,P5)
 		k+=1
+		j+=1
 		
-	j+=1
+
 
 #start of Object 6 - Control Inputs ID = 4 for all Control inputs MCD and SS or NL
-object6 = etree.Element("OBJECT", Id = "4", Key = "148")
+object6 = etree.Element("OBJECT", Id = "4", Key = str(objGroupNums))
 object6.text = "\n"
 object6.tail = "\n"
 device2.insert(3, object6)
+objGroupNums += 1
+
+a = rows2 // 16
+e = rows2 % 16
+controlNumGroups = a
+if e != 0:
+	controlNumGroups += 1
+
+# for putting all the control points in a list
+for x in range(rows2):
+	cellValue = sheet.cell_value(x,2)
+	if cellValue != "":
+		controltags[x] = cellValue
+	else:
+		controltags[x] = cellValue
+
+a = 0
+h = 0
+j = 0
+k = 0
+for i in range(controlNumGroups):
+	controlGroups = etree.Element("GROUP", Id=str(i+1), Key=str(objGroupNums))
+	controlGroups.text = "\n"
+	controlGroups.tail = "\n"
+	object6.insert(h, controlGroups)
+	objGroupNums += 1
+
+	for h in range(16):
+
+		if controltags[k] == "UNDEFINED":
+			P6 = etree.Element("P", Id=str(h), Key=str(controlNums[j]))
+			P6.text = ""
+			P6.tail = "\n"
+			controlGroups.insert(h, P6)
+		elif controltags[k] != "":
+			P6 = etree.Element("P", Id=str(h), Key=str(controlNums[j]), Name=str(controltags[k]).replace(" ", "_").replace("\\", "_").replace("//", "_").replace("-", "_").upper() + "_" + str(sheet.cell_value(a,5)))
+			P6.text = ""
+			P6.tail = "\n"
+			controlGroups.insert(h, P6)
+		if sheet.cell_value(a, 5) == "DISABLE":
+			invert = etree.Element("Invert")
+			invert.text = "0"
+			invert.tail = "\n"
+			P6.insert(0,invert)
+			P6.text = "\n"
+		a += 1
+		k += 1
+		j += 1
 
 
 
 
-			
+#start of Object 7 - Binary Inputs ID = 4 for all Control inputs MCD and SS or NL
+object7 = etree.Element("OBJECT", Id = "7", Key = str(objGroupNums))
+object7.text = "\n"
+object7.tail = "\n"
+device2.insert(4, object7)
+objGroupNums += 1
+
+#start of Group 7 - Binary Inputs ID = 0 for all Control inputs MCD and SS or NL
+group7 = etree.Element("GROUP", Id = "0", Key = str(objGroupNums))
+group7.text = "\n"
+group7.tail = "\n"
+object7.insert(0, group7)
+objGroupNums += 1
+
+for i in range(rows):
+
+	#checks if we have reached the end of the CD type binaries
+	if b[i] == "internal":
+		break
+	#checks if the cell is either empty or a NL binary point
+	if b[i] == None:
+		P7 = etree.Element("P", Id=str(l), Key=str(binaryNums[l]))
+		P7.text = ""
+		P7.tail = "\n"
+		group7.insert(i, P7)
+		l += 1
+
+	elif b[i] != "CD":
+		P7 = etree.Element("P", Id=str(l), Key=str(binaryNums[l]), Name=str(binarytags[l]).replace(" ", "_").replace("\\", "_").replace("//", "_").replace("-","_").upper())
+		P7.text = ""
+		P7.tail = "\n"
+		P7.tail = "\n"
+		group7.insert(i, P7)
+		l += 1
+
+#start of Object 8 - Binary Inputs ID = 4 for all Control inputs MCD and SS or NL
+object8 = etree.Element("OBJECT", Id = "16384", Key = str(objGroupNums))
+object8.text = "\n"
+object8.tail = "\n"
+device2.insert(5, object8)
+
+P8 = etree.Element("P", Id = "0", Key=str(binaryNums[l]), Name=str(binarytags[l]).replace(" ", "_").replace("\\", "_").replace("//", "_").replace("-","_").upper())
+P8.text = ""
+P8.tail = "\n"
+object8.insert(0, P8)
+
+
 tree = etree.ElementTree(root)
 tree.write("sample.xml")
 #add for addtional info - - - > ,xml_declaration=True,   encoding="utf-8")  
